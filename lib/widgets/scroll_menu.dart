02877132603e7f95bea4a5bee2c689c3e0bcb1f5
Eliana Miller <elianasmiller@gmail.com>
2020-03-26 12:55:31 -0400

    edited icons bar

--- lib/widgets/scroll_menu.dart
+++ lib/widgets/scroll_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
+import 'image_editor.dart';
+import 'edited_image.dart';
 
 class MyHomePage extends StatelessWidget {
 @override
@@ -16,7 +17,6 @@ Widget build(BuildContext context) {
 }
 
 class CircleImages extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return CircleWidgets();
@@ -25,39 +25,13 @@ class CircleImages extends StatefulWidget {
 
 
 class CircleWidgets extends State<CircleImages> {
-
-  // CircleWidgets({Key key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Material(
-//       color: Colors.white,
-//       child: Center(
-//         child: Ink(
-//           decoration: const ShapeDecoration(
-//             color: Colors.lightBlue,
-//             shape: CircleBorder(),
-//           ),
-//           child: IconButton(
-//             icon: Icon(Icons.android),
-//             color: Colors.white,
-//             onPressed: () {},
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
   Widget _placeholderIcon() {
-    
     return GestureDetector(
       // When the child is tapped, show a snackbar.
-      onTap: () {
-        
-        final snackBar = SnackBar(content: Text("Grayscale"));
-        Scaffold.of(context).showSnackBar(snackBar);
-      },
+      // onTap: () {
+      //   final snackBar = SnackBar(content: Text("Crop"));
+      //   Scaffold.of(context).showSnackBar(snackBar);
+      // },
       child: 
       Material(
       color: Colors.white,
@@ -65,9 +39,7 @@ class CircleWidgets extends State<CircleImages> {
         child: Ink(
           decoration: const ShapeDecoration(
             color: Colors.lightBlue,
-            shape: CircleBorder(
-              side: BorderSide(color: Colors.red)
-            ),
+            shape: CircleBorder(),
           ),
           child: IconButton(
             icon: Icon(
@@ -77,53 +49,70 @@ class CircleWidgets extends State<CircleImages> {
             ),
             color: Colors.white,
             onPressed: () {
-              // final snackBar = SnackBar(content: Text("Crop"));
-              // Scaffold.of(context).showSnackBar(snackBar);
               EditedImage image = GetIt.I<EditedImage>();
-              image.filter = Grayscale();
+              //  final snackBar = SnackBar(content: Text("Crop"));
+              // Scaffold.of(context).showSnackBar(snackBar);
             },
           ),
         ),
       ),
     ),
-        // Icon(
-        // Icons.audiotrack,
-        // color: Colors.green,
-        // size: 50.0,
-        // ),
-      //  Container(
-      //   height: 60.0,
-      //   width: 60.0,
-      //   margin: EdgeInsets.all(6.0),
-      //   decoration: BoxDecoration(
-      //     borderRadius: BorderRadius.circular(100.0),
-      //     boxShadow: [
-      //       new BoxShadow(
-      //         color: Color.fromARGB(100, 0, 0, 0),
-      //         blurRadius: 5.0,
-      //         offset: Offset(5.0, 5.0),
-      //       ),
-      //     ],
-      //     border: Border.all(
-      //       width: 2.0,
-      //       style: BorderStyle.solid,
-      //       color: Color.fromARGB(255, 0, 0, 0),
-      //     ),
-      //     image: DecorationImage(
-      //       fit: BoxFit.cover,
-      //       image: musicIcon()
-      //       // image: NetworkImage(
-      //       //   "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/1200px-Noto_Emoji_KitKat_263a.svg.png",
-      //       ),
-      //     ),
-      //   ),
-      // ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     List<Widget> widgets = new List.generate(20, (index) => _placeholderIcon());
+    return Container(
+      height: 80.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.all(2.0),
+        children: widgets,
+      ),
+    );
+  }
+}
+
+
+class SubMenu extends State<CircleImages> {
+  Widget _placeholderIcon() {
+    return GestureDetector(
+      // When the child is tapped, show a snackbar.
+      // onTap: () {
+      //   final snackBar = SnackBar(content: Text("Submenu"));
+      //   Scaffold.of(context).showSnackBar(snackBar);
+      // },
+      child: 
+      Material(
+      color: Colors.white,
+      child: Center(
+        child: Ink(
+          decoration: const ShapeDecoration(
+            color: Colors.green,
+            shape: CircleBorder(),
+          ),
+          child: IconButton(
+            icon: Icon(
+              Icons.crop,
+              color: Colors.yellow,
+              size: 30.0,
+            ),
+            color: Colors.white,
+            onPressed: () {
+              final snackBar = SnackBar(content: Text("Crop"));
+              Scaffold.of(context).showSnackBar(snackBar);
+            },
+          ),
+        ),
+      ),
+    ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgets = new List.generate(3, (index) => _placeholderIcon());
     return Container(
       height: 80.0,
       child: ListView(
