d88c1e5488538c5ba424abb2eff994c19245fd05
Nikki <nicolette.j.morris@gmail.com>
2020-03-29 21:46:48 -0400

    destash

--- lib/widgets/scanner.dart
+++ lib/widgets/scanner.dart
@@ -12,7 +12,6 @@ import 'detector_painters.dart';
 import 'image_editor.dart';
 import '../store/edited_image.dart';
 
-<<<<<<< Updated upstream
 // Main Scanner class
 class PictureScanner extends StatefulWidget {
   @override
@@ -21,6 +20,8 @@ class PictureScanner extends StatefulWidget {
 
 // Scanner class local state
 class _PictureScannerState extends State<PictureScanner> {
+final EditedImage imageStore = GetIt.I<EditedImage>();
+final ScannedFace scannedFace = GetIt.I<ScannedFace>();
   File _imageFile; // The selected image
   Size _imageSize; // The image size
   dynamic _scanResults;
@@ -36,7 +37,7 @@ class _PictureScannerState extends State<PictureScanner> {
       _imageSize = null;
     });
 
-    final File imageFile = 
+    final File imageFile =
 
     if (imageFile != null) {
       _getImageSize(imageFile);
@@ -89,28 +90,6 @@ class _PictureScannerState extends State<PictureScanner> {
 
     return CustomPaint(
       painter: painter,
-=======
-final EditedImage imageStore = GetIt.I<EditedImage>();
-final ScannedFace scannedFace = GetIt.I<ScannedFace>();
-
-class PictureScanner extends StatelessWidget {
-  final FaceDetector _faceDetector = FirebaseVision.instance.faceDetector(
-    FaceDetectorOptions(
-      enableContours: true,
-      mode: FaceDetectorMode.fast,
-    ),
-  );
-
-  Widget _buildImage(BuildContext context) {
-    return Observer(
-      builder: (context) {
-        return Container(
-          child: CustomPaint(
-            painter: FaceDetectorSquare(),
-          ),
-        );
-      },
->>>>>>> Stashed changes
     );
   }
 
@@ -151,48 +130,7 @@ class PictureScanner extends StatelessWidget {
 
   void dispose() {
     _faceDetector.close();
-<<<<<<< Updated upstream
 
     super.dispose();
-=======
-  }
-}
-
-class FaceDetectorSquare extends CustomPainter {
-  Size absoluteImageSize;
-  List<Face> faces;
-
-  FaceDetectorSquare({this.absoluteImageSize, this.faces});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    if (absoluteImageSize = null) absoluteImageSize = imageStore.size;
-    if (faces = null) faces = scannedFace.scannedFaces;
-
-    final double scaleX = size.width / absoluteImageSize.width;
-    final double scaleY = size.height / absoluteImageSize.height;
-
-    for (Face face in faces) {
-      canvas.drawRect(
-        Rect.fromLTRB(
-          face.boundingBox.left * scaleX,
-          face.boundingBox.top * scaleY,
-          face.boundingBox.right * scaleX,
-          face.boundingBox.bottom * scaleY,
-        ),
-        Paint(),
-      );
-
-      FaceContourType contour = FaceContourType.face;
-      ui.PointMode pointMode = ui.PointMode.points;
-      List<Offset> pointList = face.getContour(contour).positionsList;
-      canvas.drawPoints(pointMode, pointList, Paint());
-    }
-  }
-
-  @override
-  bool shouldRepaint(FaceDetectorSquare old) {
-    return old.absoluteImageSize != absoluteImageSize || old.faces != faces;
->>>>>>> Stashed changes
   }
 }
