4be19f9e403c97eaa48390a50f0600928462795b
Eliana Miller <elianasmiller@gmail.com>
2020-04-06 17:23:41 -0400

    standardized theme color

--- lib/widgets/scroll_menu.dart
+++ lib/widgets/scroll_menu.dart
@@ -27,23 +27,44 @@ class MenuWidget extends StatefulWidget {
 }
 
 class _MenuWidgetState extends State<MenuWidget> {
+
+  // int _selectedIndex = 0;
+  // static const TextStyle optionStyle =
+  //     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  // static List _filterOptions = [
+  //   SubMenuWidget(),
+  //   cropImage(),
+  //   // Navigator.pushNamed(context, '/stickers');
+  //   // savePng()
+  // ];
+
+  // void _onItemTapped(int index) {
+  //   setState(() {
+  //     _selectedIndex = index;
+  //   });
+  //   // _selectedIndex = index;
+  //   // image.filter = _filterOptions.elementAt(_selectedIndex);
+  // }
+
+//ORIGINAL 
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static List<Widget> _widgetOptions = <Widget>[
-    SubMenuWidget(),
-    Text(
-      'Choose your crop',
-      style: optionStyle,
-    ),
-    Text(
-      "You should be decorating!",
-      style: optionStyle,
-    )
-    // Stickers(),
-  ];
+  // static List<Widget> _widgetOptions = <Widget>[
+  //   SubMenuWidget(),
+  //   Text(
+  //     'Choose your crop',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     "You should be decorating!",
+  //     style: optionStyle,
+  //   )
+  //   // Stickers(),
+  // ];
 
   void _onItemTapped(int index) {
+    if (index == 0) SubMenuWidget();
     if (index == 1) cropImage();
     if (index == 2) Navigator.pushNamed(context, '/stickers');
     if (index == 3) savePng();
@@ -55,10 +76,12 @@ class _MenuWidgetState extends State<MenuWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
+      body: Center (child: SubMenuWidget()),
+      // body: Center(
+      //   child: _widgetOptions.elementAt(_selectedIndex),
+      // ),
       bottomNavigationBar: BottomNavigationBar(
+       type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.filter),
@@ -108,6 +131,9 @@ class _SubMenuWidgetState extends State<SubMenuWidget> {
   ];
 
   void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
     _selectedIndex = index;
     image.filter = _filterOptions.elementAt(_selectedIndex);
   }
@@ -122,26 +148,30 @@ class _SubMenuWidgetState extends State<SubMenuWidget> {
       //   child: _widgetOptions.elementAt(_selectedIndex),
       // ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.collections, color: Colors.grey),
+            icon: Icon(Icons.collections),
             title: Text('Grayscale'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.collections, color: Colors.brown),
+            icon: Icon(Icons.collections),
             title: Text('Sepia'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.collections, color: Colors.black),
+            icon: Icon(Icons.collections),
             title: Text('Pixelate'),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.collections, color: Colors.green),
-            title: Text('Fill'),
+             BottomNavigationBarItem(
+            icon: Icon(Icons.collections),
+            title: Text('coming soon'),
           ),
         ],
-        // currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue[800],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.deepPurple,
+        backgroundColor: Colors.white,
+        // selectedItemColor: Colors.blue[800],
         onTap: _onItemTapped,
       ),
     );
